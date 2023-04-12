7fa7234db9a98127723afd9561cd9598ff966772
MaheshMelpati <maheshance12@gmail.com>
2023-04-12 20:28:49 +0530

    home

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 import EmpData from './components/EmpDetails/EmpData';
 import EmpNew from './components/EmpDetails/EmpNew';
 import EmpUpdate from './components/EmpDetails/EmpUpdate';
+import Home from './components/Home/Home';
 
 function App() {
   return (
   <>
   <Routes>
-    <Route path='/' element={<EmpData />} />
+    <Route path='/' element={<Home />} />
+    <Route path='/emp' element={<EmpData />} />
     <Route path='emp-new' element={<EmpNew />} />
     <Route path='emp-update' element={<EmpUpdate />} />
   </Routes>
